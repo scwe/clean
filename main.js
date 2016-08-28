ba58c5724d10587ae121ff899234b735ef12be34
Scott <weston.scot@gmail.com>
2016-08-28 18:10:17 +0200

    Lots of work getting inital torrent downloading working

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-const {app, BrowserWindow} = require('electron')
-const Bencoding = require('bencoding')
+const {app, BrowserWindow, ipcMain, dialog} = require('electron')
 
-let win
+var parseTorrent = require('parse-torrent');
+var fs = require('fs');
+var torrent = require('torrent-stream');
+
+let win;
 
 function createWindow(){
     win = new BrowserWindow({width: 800, height: 600});
@@ -24,6 +27,26 @@ app.on('window-all-closed', () => {
         app.quit();
     }
 
+});
+
+function loadTorrents(filenames){
+
+    if(filenames){
+        console.log("Woohoo we got this far: "+filenames);
+        for(var key in filenames){
+            var tFile = parseTorrent(fs.readFileSync(filenames[key]));
+            var magnet = parseTorrent.toMagnetURI(tFile);
+            console.log(magnet);
+        }
+    }
+}
+
+ipcMain.on('add_torrent_files', function(event){
+    console.log("Recieved the event, trying to open dialog");
+    var torrents = dialog.showOpenDialog({
+        title: 'Open Torrent', 
+        filters: [{name: "torrent", extensions: ["torrent"]}],
+        properties: ['openFile', 'multiSelections']}, loadTorrents);
 });
 
 app.on('activate', () => {
--- scripts/loadFile.js
+++ scripts/loadFile.js
@@ -1,3 +1,6 @@
-$(
+const { ipcRenderer } = require('electron')
 
- );
+function loadFiles(){
+    console.log("Trying to load files");
+    ipcRenderer.send('add_torrent_files');
+}
