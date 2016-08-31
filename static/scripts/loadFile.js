9bead5855e5bc298c89facf0ce7a99b03b12d323
Scott <weston.scot@gmail.com>
2016-08-31 11:44:54 +0200

    Moved a lot of stuff around to react working

--- static/scripts/loadFile.js
+++ static/scripts/loadFile.js
@@ -5,6 +5,11 @@ function loadFiles(){
     ipcRenderer.send('add_torrent_files');
 }
 
+function loadFromMagnet(){
+    console.log("UNIMPLEMENTED");
+    ipcRenderer.send('add_torrent_from_magnet');
+}
+
 function cancelTorrent(){
     console.log("Canceling the torrent");
     ipcRenderer.send('cancel_torrent', undefined);
