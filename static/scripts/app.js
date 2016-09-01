2dd82400423b62586cb76c0a44a49ef4abf704c9
Scott <weston.scot@gmail.com>
2016-09-01 10:11:37 +0200

    Made Downloads the index route and fixed up the active nav bar links

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router, Route, hashHistory} from 'react-router';
+import {Router, Route, hashHistory, IndexRoute} from 'react-router';
 import App from '../views/app.jsx';
 import Downloads from '../views/downloads.jsx';
 import Watch from "../views/watch.jsx";
@@ -19,7 +19,7 @@ window.onload = function(){
     ReactDOM.render(
         <Router history={hashHistory}>
             <Route path="/" component={App}>
-                <Route path='/downloads' component={Downloads}/>
+                <IndexRoute components={Downloads}/>
                 <Route path='/watch' component={Watch}/>
                 <Route path='/stats' component={Stats}/>
                 <Route path='/settings' component={Settings}/>
--- static/views/app.jsx
+++ static/views/app.jsx
@@ -1,6 +1,6 @@
 'use babel';
 import React from 'react';
-import {Link} from 'react-router';
+import {Link, IndexLink} from 'react-router';
 
 const {electronApp} = require('electron').remote;
 
@@ -18,7 +18,7 @@ class App extends React.Component {
             <div className="navbar-brand" id="title-text" href="" onClick={this.titleClicked.bind(this)}>.clean</div>
             <div id="navbar">
                 <nav className="nav navbar-nav pull-xs-right">
-                    <Link className="nav-item nav-link" to="/downloads" activeClassName="active">Downloads</Link>
+                    <IndexLink className="nav-item nav-link" to="/" activeClassName="active">Downloads</IndexLink>
                     <Link className="nav-item nav-link" to="/stats" activeClassName="active">Stats</Link>
                     <Link className="nav-item nav-link" to="/watch" activeClassName="active">Watch</Link>
                     <Link className="nav-item nav-link" to="/settings" activeClassName="active">Settings</Link>
