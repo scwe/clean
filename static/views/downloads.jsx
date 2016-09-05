80b4686455a3bc315d93383bf6e710a7e8b83382
Scott <weston.scot@gmail.com>
2016-09-05 10:53:22 +0200

    Moved some files and folders around so I can use non-min js+css files

--- static/views/downloads.jsx
+++ static/views/downloads.jsx
@@ -10,12 +10,12 @@ class Downloads extends React.Component {
     render() {
         return (
             <div className="row">
-                <div className="col s3">
+                <div className="col s12 m4 l3">
                     <div className="container">
                         <Sidebar/>
                     </div>
                 </div>
-                <div className="col s9">
+                <div className="col s12 m8 l9">
                     <div className="container">
                         <div>
                             <TorrentButtons/>
--- static/views/navbar.jsx
+++ static/views/navbar.jsx
@@ -14,9 +14,17 @@ class Navbar extends React.Component {
         return <div>
             <div className="navbar-fixed">
                 <nav>
-                    <div className="nav-wrapper">
+                    <div className="nav-wrapper blue-grey">
                         <div className="brand-logo" id="title">.clean</div>
-                        <ul className="right hide-on-small-and-down" id="nav-links">
+                        <a href="#" data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></a>
+                        <ul className="right hide-on-med-and-down" id='nav-links'>
+                            <NavItem index={true} to="/">Downloads</NavItem>
+                            <NavItem to="/stats">Stats</NavItem>
+                            <NavItem to="/watch">Watch</NavItem>
+                            <NavItem to="/settings">Settings</NavItem>
+                            <li><a onClick={this.quitClicked.bind(this)}>Quit</a></li>
+                        </ul>
+                        <ul className="side-nav" id="mobile-demo">
                             <NavItem index={true} to="/">Downloads</NavItem>
                             <NavItem to="/stats">Stats</NavItem>
                             <NavItem to="/watch">Watch</NavItem>
@@ -25,6 +33,7 @@ class Navbar extends React.Component {
                         </ul>
                     </div>
                 </nav>
+
             </div>
             {this.props.children}
         </div>;
