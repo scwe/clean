5070c5b2fb6e377f4e78f436af7fca1a3a737a8c
Scott <sw@colortv.com>
2016-10-15 19:51:16 +1300

    Style progress bad to be flat

--- static/views/torrentItem.jsx
+++ static/views/torrentItem.jsx
@@ -38,28 +38,30 @@ class TorrentItem extends React.Component {
             width: (this.props.torrent.progress * 100).toString()+"%"
         };
 
-        return <li>
-            <div className="collapsible-header">
-                <div className="col s12">
-                    <div className="flow-text">
-                        <div className="center-align wrap-word small-flow">{this.props.torrent.name}</div>
-                    </div>
-                </div>
-                <div className="col s6">
-                    <i className="material-icons left">file_download</i>
-                    {this.props.torrent.dataRate.download}
-                </div>
-                <div className="col s6">
-                    <i className="material-icons left">file_upload</i>
-                    {this.props.torrent.dataRate.upload}
-                </div>
+      return (
+        <li>
+          <div className="collapsible-header">
+            <div className="col s12">
+              <div className="flow-text">
+                <div className="center-align wrap-word small-flow">{this.props.torrent.name}</div>
+              </div>
+            </div>
+            <div className="col s6">
+              <i className="material-icons left">file_download</i>
+              {this.props.torrent.dataRate.download}
+            </div>
+            <div className="col s6">
+              <i className="material-icons left">file_upload</i>
+              {this.props.torrent.dataRate.upload}
+            </div>
 
-                    <div className="progress">
-                        <div className="determinate" style={progressStyle}></div>
-                    </div>
-                </div>
-                <div className="collapsible-body"><p>{this.props.torrent.state}</p></div>
-            </li>;
+          </div>
+            <div className="progress">
+              <div className="determinate" style={progressStyle}></div>
+            </div>
+          <div className="collapsible-body"><p>{this.props.torrent.state}</p></div>
+        </li>
+      )
     }
 }
 
